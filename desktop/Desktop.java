84ff1efe6e3612e1f2cbf4f06a83b40d4bd4f48d
James Browning <brownij6@wwu.edu>
2020-08-01 03:30:15 -0700

    creating desktop opencv test

--- desktop/Desktop.java
+++ desktop/Desktop.java
@@ -1,5 +1,9 @@
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.VideoWriter;
 import org.opencv.core.Core;
+import org.opencv.core.Size;
+import org.opencv.videoio.Videoio;
+import org.opencv.core.Mat;
 
 public class Desktop{
    
@@ -7,14 +11,26 @@ public class Desktop{
       
       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
             
-      //open video file specified by command-line argument
+      //open video file specified by command-line argument 0
       VideoCapture src = new VideoCapture(args[0]);
-      VideoWriter vw = new VideoWriter();
+      
+      Size frameSize = new Size((int) src.get(Videoio.CAP_PROP_FRAME_WIDTH), (int) src.get(Videoio.CAP_PROP_FRAME_HEIGHT));
+
+      //output file specified by command-line argument 1
+      VideoWriter vw = new VideoWriter(args[1], VideoWriter.fourcc('x', '2','6','4'),
+                                       src.get(Videoio.CAP_PROP_FPS), frameSize, true);
+      
+      Mat mat = new Mat();                 
+      while (src.read(mat)) {
+         vw.write(mat);         
+      }
       
       if(!src.isOpened()){
          System.out.println("Failed to open video file.");
          System.exit(0);
       }
+      
+      
    }
    
 }
