51e5dd7ded140f0ca9cfcbb02277cfd603b7fad8
James Browning <brownij6@wwu.edu>
2020-08-01 02:32:05 -0700

    creating desktop opencv test

--- desktop/Desktop.java
+++ desktop/Desktop.java
@@ -1,3 +1,20 @@
-public class Desktop{
+import org.opencv.videoio.VideoCapture;
+import org.opencv.core.Core;
 
+public class Desktop{
+   
+   public static void main(String[] args){
+      
+      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+            
+      //open video file specified by command-line argument
+      VideoCapture src = new VideoCapture(args[0]);
+      VideoWriter vw = new VideoWriter();
+      
+      if(!src.isOpened()){
+         System.out.println("Failed to open video file.");
+         System.exit(0);
+      }
+   }
+   
 }
