5c085cd85cda327022f1059c1a6b4e3d1e9674b2
James Browning <brownij6@wwu.edu>
2020-08-22 19:47:15 -0700

    added profile detection and audio

--- desktop/Desktop.java
+++ desktop/Desktop.java
@@ -1,8 +1,8 @@
 
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.lang.*; 
+import java.io.*; 
+import java.util.Scanner;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
@@ -25,6 +25,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
+
+
 public class Desktop{
    
    public static void main(String[] args){
@@ -33,9 +35,12 @@ public class Desktop{
             
       //open video file specified by command-line argument 0
       VideoCapture src = new VideoCapture(args[0]);
+           
+      CascadeClassifier ccFront = new CascadeClassifier();
+      ccFront.load("./resources/haarcascade_frontalface_default.xml");
       
-      CascadeClassifier cc = new CascadeClassifier();
-      cc.load("./resources/haarcascade_frontalface_default.xml");
+      CascadeClassifier ccProf = new CascadeClassifier();
+      ccProf.load("./resources/haarcascade_profileface.xml");
       
        if(!src.isOpened()){
          System.out.println("Failed to open video file.");
@@ -45,7 +50,7 @@ public class Desktop{
       Size frameSize = new Size((int) src.get(Videoio.CAP_PROP_FRAME_HEIGHT), (int) src.get(Videoio.CAP_PROP_FRAME_WIDTH));
 
       //output file specified by command-line argument 1
-      VideoWriter vw = new VideoWriter(args[1], VideoWriter.fourcc('x', '2','6','4'),
+      VideoWriter vw = new VideoWriter("./tests/tmp.avi", VideoWriter.fourcc('x', '2','6','4'),
                                        src.get(Videoio.CAP_PROP_FPS), frameSize, true);
       
       Mat mat = new Mat(); 
@@ -60,23 +65,91 @@ public class Desktop{
          
         if (faceSize == 0){
    			int height = gray.rows();
-   			if (Math.round(height * 0.2f) > 0){
-   				faceSize = Math.round(height * 0.2f);
+   			if (Math.round(height * 0.15f) > 0){
+   				faceSize = Math.round(height * 0.15f);
    			}
 		  }
         
-        cc.detectMultiScale(gray, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
+        ccFront.detectMultiScale(gray, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
 				new Size(faceSize, faceSize), new Size());
             
         Rect[] facesArray = faces.toArray();
 		  for (int i = 0; i < facesArray.length; i++){
-			   Imgproc.rectangle(tmp, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 3);
+			   Imgproc.rectangle(tmp, facesArray[i].tl(), facesArray[i].br(), new Scalar(255, 255, 255), -1);
+         }
+         
+        ccProf.detectMultiScale(gray, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
+				new Size(faceSize, faceSize), new Size());
+            
+        facesArray = faces.toArray();
+		  for (int i = 0; i < facesArray.length; i++){
+			   Imgproc.rectangle(tmp, facesArray[i].tl(), facesArray[i].br(), new Scalar(255, 255, 255), -1);
          }
          
          vw.write(tmp);         
       }
       
+      String[] commands = (System.getProperty("user.dir")+"\\ffmpeg-4.3.1-win64-static\\bin\\" + "ffmpeg -i "+ args[0]+" -vn -acodec copy .\\tests\\tmp.aac").split(" ");
+      ProcessBuilder pb = new ProcessBuilder(commands);
+      pb.redirectErrorStream(true);
       
+      
+      try{
+         Process p = pb.start();
+         InputStream is = p.getInputStream();
+         OutputStream os = p.getOutputStream();
+         os.close();
+         is.close();
+         p.waitFor();
+         p.destroy();
+      }catch(Exception e){
+         System.out.println("exception: " + e);
+      }
+      
+      commands = (System.getProperty("user.dir")+"\\ffmpeg-4.3.1-win64-static\\bin\\" + "ffmpeg -i .\\tests\\tmp.avi -i .\\tests\\tmp.aac -c:v copy -map 0:v:0 -map 1:a:0 -c:a aac -b:a 192k "+ args[1]).split(" ");
+      pb = new ProcessBuilder(commands);
+      pb.redirectErrorStream(true);
+      
+      
+      try{
+         Process p = pb.start();
+         InputStream is = p.getInputStream();
+         OutputStream os = p.getOutputStream();
+         os.close();
+         is.close();
+         p.waitFor();
+         p.destroy();
+      }catch(Exception e){
+         System.out.println("exception: " + e);
+      }
+      
+      vw.release();
+      File tmp = new File(System.getProperty("user.dir")+"\\tests\\tmp.avi");
+      File tmp2 = new File(System.getProperty("user.dir")+"\\tests\\tmp.aac");
+
+            
+      try{
+         System.gc();
+         if(tmp.delete()){
+            System.out.println("true");
+         }
+         if(tmp2.delete()){
+            System.out.println("true");
+         }
+         
+      }catch(Exception e){
+         System.out.println(e);
+      }
+         
    }
    
+   public static void flushInputStreamReader (Process process) throws IOException, InterruptedException {
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line=null;
+            StringBuilder s = new StringBuilder();
+            while(input.ready() && (line=input.readLine()) != null) {            
+                s.append(line);
+            }
+   }
 }
+
